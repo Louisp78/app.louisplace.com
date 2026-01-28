381e07989712e0f9474964e42ae5d46bd9848133
Louis Place <louis.place@epita.fr>
2026-01-28 21:16:33 +0100

    refactor: delete unused web config and use spring security cors config instead

--- backend/src/main/java/com/louisplace/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/louisplace/backend/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.louisplace.backend.config;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -9,10 +12,16 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
         static final int MAX_SESSION_NUMBER = 1;
+
+        @Value("${spring.config.clientUrl}")
+        private String clientUrl;
 
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -40,5 +49,19 @@ public class SecurityConfig {
                 return username -> {
                         throw new UsernameNotFoundException("Not supported");
                 };
+        }
+
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowedOrigins(Arrays.asList(clientUrl));
+                configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Accept", "X-Requested-With", "X-XSRF-TOKEN"));
+                configuration.setAllowCredentials(true);
+                configuration.setMaxAge(3600L);
+
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+                return source;
         }
 }
